22033e5b5773c57c83ea9b5588b4826f47f42478
jjy <jjyruby@gmail.com>
2013-08-10 00:20:19 +0800

    actor\!

--- lib/pb_actor.rb
+++ lib/pb_actor.rb
@@ -1,5 +1,24 @@
+# -*- coding: utf-8 -*-
 require "pb_actor/version"
+require "pb_actor/proxy"
 
 module PbActor
-  # Your code goes here...
+  class << self
+    def included base
+      base.send :include, InstanceMethods
+      base.send :extend, ClassMethods
+    end
+  end
+
+  module ClassMethods
+    def new *args, &blk
+      origin = allocate
+      proxy = Proxy.new origin
+      proxy.async.send :initialize, *args, &blk
+      proxy
+    end
+  end
+
+  module InstanceMethods
+  end
 end
---
+++ lib/pb_actor/proxy.rb
@@ -0,0 +1,111 @@
+require 'timeout'
+
+module PbActor
+  class DeadActorError < StandardError
+  end
+
+  class BasicProxy
+    module Message
+      class << self
+        def send msg, wr
+          Marshal.dump(msg, wr)
+        end
+
+        def recv rd
+          Marshal.load rd
+        rescue
+          puts "error"
+        end
+      end
+    end
+
+    def alive?
+      begin
+        # Have any other way to check a process status?
+        timeout(0.001){Process.wait}
+      rescue Timeout::Error => e
+      end
+      Process.kill(0, @pid) == 1
+    rescue Errno::ESRCH, Errno::ECHILD => e
+      false
+    end
+
+    def method_missing method, *args, &blk
+      raise ArgumentError, 'actor not support block' if blk
+      raise DeadActorError, 'dead actor call' unless alive?
+    end
+
+    def to_s
+      "#{self.class}(#{@origin.class})"
+    end
+
+    undef send, public_send
+  end
+
+  class Proxy < BasicProxy
+    def initialize origin
+      @origin = origin
+      pr, cw = IO.pipe
+      cr, pw = IO.pipe
+      @pid = fork do
+        [pr, pw].each &:close
+        Signal.trap("HUP") {
+          puts "#{@pid} terminate"
+          exit
+        }
+        loop do
+          type, method, *args = Message.recv cr
+          case type
+          when :method_call, :async_method_call
+            value = @origin.public_send method, *args
+            Message.send([:return_value, value], cw) if type == :method_call
+          when :terminate
+            exit
+          else
+            raise "what happend!? receive #{type.inspect}"
+          end
+        end
+      end
+      [cr, cw].each &:close
+      @rd = pr
+      @wr = pw
+    end
+
+    def method_missing method, *args, &blk
+      super
+      Message.send([:method_call, method, *args], @wr)
+      type, value = Message.recv @rd
+      case type
+      when :return_value
+        value
+      else
+        raise "what happend!? receive #{type}"
+      end
+    end
+
+    def async
+      AsyncProxy.new @origin, @pid, @wr
+    end
+
+    def terminate
+      Message.send [:terminate], @wr
+      Process.wait @pid
+    end
+
+    def terminate!
+      Process.kill "HUP", @pid
+    end
+  end
+
+  class AsyncProxy < BasicProxy
+    def initialize origin, pid, wr
+      @origin, @pid, @wr = origin, pid, wr
+    end
+
+    def method_missing method, *args, &blk
+      super
+      Message.send [:async_method_call, method, *args], @wr
+      nil
+    end
+  end
+end
