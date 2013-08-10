e8fb91d1e76be06378dec2a40dd3f0e270f773a8
jjy <jjyruby@gmail.com>
2013-08-10 08:07:18 +0800

    support_future

--- lib/pb_actor/proxy.rb
+++ lib/pb_actor/proxy.rb
@@ -1,22 +1,27 @@
 require 'timeout'
+require 'securerandom'
 
 module PbActor
   class DeadActorError < StandardError
   end
 
-  class BasicProxy
-    module Message
-      class << self
-        def send msg, wr
-          Marshal.dump(msg, wr)
-        rescue Errno::EPIPE => e
-          raise DeadActorError, 'dead actor call'
-        end
-
-        def recv rd
-          Marshal.load rd
-        end
+  module Message
+    class << self
+      def send msg, wr
+        Marshal.dump(msg, wr)
+      rescue Errno::EPIPE => e
+        raise DeadActorError, 'dead actor call'
       end
+
+      def recv rd
+        Marshal.load rd
+      end
+    end
+  end
+
+  class BasicProxy
+    def initialize origin, pid, wr, rd
+      @origin, @pid, @wr, @rd = origin, pid, wr, rd
     end
 
     def alive?
@@ -49,12 +54,20 @@ module PbActor
       cr, pw = IO.pipe
       @pid = fork do
         [pr, pw].each &:close
+        @future_values = {}
         loop do
-          type, method, *args = Message.recv cr
+          type, id, method, *args = Message.recv cr
           case type
-          when :method_call, :async_method_call
-            value = @origin.public_send method, *args
-            Message.send([:return_value, value], cw) if type == :method_call
+          when :async_method_call
+            @origin.public_send method, *args
+          when :future_method_call
+            @future_values[id] = @origin.public_send method, *args
+          when :future_value_get
+            Message.send(if @future_values.has_key? id
+              [:future_value, @future_values.delete(id)]
+            else
+              [:no_value]
+            end, cw)
           when :terminate
             exit
           else
@@ -69,18 +82,15 @@ module PbActor
 
     def method_missing method, *args, &blk
       super
-      Message.send([:method_call, method, *args], @wr)
-      type, value = Message.recv @rd
-      case type
-      when :return_value
-        value
-      else
-        raise "what happend!? receive #{type}"
-      end
+      future.method_missing(method, *args).value
     end
 
     def async
-      AsyncProxy.new @origin, @pid, @wr
+      AsyncProxy.new @origin, @pid, @wr, @rd
+    end
+
+    def future
+      FutureProxy.new @origin, @pid, @wr, @rd
     end
 
     def terminate
@@ -97,14 +107,39 @@ module PbActor
   end
 
   class AsyncProxy < BasicProxy
-    def initialize origin, pid, wr
-      @origin, @pid, @wr = origin, pid, wr
-    end
-
     def method_missing method, *args, &blk
       super
-      Message.send [:async_method_call, method, *args], @wr
+      Message.send [:async_method_call, nil, method, *args], @wr
       nil
+    end
+  end
+
+  class FutureProxy < BasicProxy
+    def method_missing method, *args, &blk
+      super
+      id = SecureRandom.uuid
+      Message.send [:future_method_call, id, method, *args], @wr
+      Future.new id, @wr, @rd
+    end
+  end
+
+  class Future
+    def initialize id, wr, rd
+      @id = id
+      @wr= wr
+      @rd = rd
+    end
+
+    def value
+      loop do
+        Message.send [:future_value_get, @id], @wr
+        type, value = Message.recv @rd
+        if type == :future_value
+          break value
+        else
+          sleep 0.01
+        end
+      end
     end
   end
 end
